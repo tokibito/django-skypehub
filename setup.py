f7e2e7ffe43a12cd3d7d0ae860b97feeeecf63f0
Shinya Okano <shinya.okano@beproud.jp>
2011-03-24 09:49:43 +0900

    version 0.2.0

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
 
 setup(
     name='django-skypehub',
-    version='0.1.1',
+    version='0.2.0',
     description='Django-skypehub is an app to use skype in remote api',
     author='Shinya Okano',
     author_email='tokibito@gmail.com',
