bf4fc89dabe4856de40bf4d8c9e96b674a1ff813
Shinya Okano <tokibito@gmail.com>
2011-10-19 04:05:15 +0900

    インデックスの設定を追加

--- skypehub/models.py
+++ skypehub/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Message(models.Model):
     body = models.TextField(u'本文')
-    sender = models.CharField(u'送信者', max_length=200)
-    chat_name = models.CharField(u'チャット名', max_length=100)
-    ctime = models.DateTimeField(u'作成日時', auto_now_add=True)
+    sender = models.CharField(u'送信者', max_length=200, db_index=True)
+    chat_name = models.CharField(u'チャット名', max_length=100, db_index=True)
+    ctime = models.DateTimeField(u'作成日時', auto_now_add=True, db_index=True)
 
     def __unicode__(self):
         return self.body
